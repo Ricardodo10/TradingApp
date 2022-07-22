8c905a0d40272799d09e8b2a5283f63b562d4aa1
ricardodo10 <ricardocoutinhosilva1999@gmail.com>
2022-07-22 10:57:31 +0200

    code

---
+++ src/Register.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+export default function Register() {
+  return (
+    <div>
+       <h1>Register</h1>
+       <form>
+  <label>
+    Nom :
+    <input type="text" name="name" />
+  </label>
+  <label>
+    Last name:
+    <input type="text" lname="lname" />
+  </label>
+  <label>
+    Username
+    <input type="text" Uname="Uname" />
+  </label>
+
+
+
+
+  <input type="submit" value="Envoyer" />
+</form>
+
+
+    </div>
+  )
+}
